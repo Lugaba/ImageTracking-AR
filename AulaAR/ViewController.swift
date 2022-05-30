88b9342c308577fa4bbbb38493b7bbd8837235c6
Luca Hummel <luca.hummel12@gmail.com>
2022-05-30 16:07:03 -0300

    Initial Commit

---
+++ AulaAR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AulaAR
+//
+//  Created by Luca Hummel on 30/05/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
